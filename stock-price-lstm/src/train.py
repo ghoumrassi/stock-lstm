2ff1b24d30446b67971db9821c43c53a4c797580
Adam <aghoumrassi@hotmail.co.uk>
2020-04-28 11:36:38 +0100

    Fixed bug

--- stock-price-lstm/src/train.py
+++ stock-price-lstm/src/train.py
@@ -114,12 +114,10 @@ class TrainModel:
         ).to_csv('predictions.csv')
 
     def predict(self):
-        test_loader = DataLoader(self.test_data, batch_size=self.batch_size, shuffle=False, drop_last=True)
-        print(len(self.test_data))
-        print(len(test_loader))
+        val_loader = DataLoader(self.val_data, batch_size=self.batch_size, shuffle=False, drop_last=True)
         with torch.set_grad_enabled(False):
             predictions_list = []
-            for X, y in test_loader:
+            for X, y in val_loader:
                 print(y)
                 out = self.model(X)
                 y_pred = out[:, -1, :].view(self.batch_size, -1)
